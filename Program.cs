9775a092a06c17efdaafee0669ab95d83eda419a
Dan Leeaphon <dleeapho@microsoft.com>
2016-03-11 23:30:56 +0000

    initial code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\x1B[31m"+"Hello World!"+"\x1B[39m");
+        }
+    }
+}
